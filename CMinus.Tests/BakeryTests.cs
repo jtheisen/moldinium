0cbc6b7e80031d83b2f220b73175f84ecba701fc
jtheisen <jens.theisen@github.com>
2023-01-10 18:53:26 +0100

    Wrote a (failing) test for the remaining feature of implementing mixin interfaces

--- CMinus.Tests/BakeryTests.cs
+++ CMinus.Tests/BakeryTests.cs
@@ -1,5 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.ComponentModel;
 
 namespace CMinus.Tests;
 
@@ -29,7 +30,7 @@ public class BakeryTests
     }
 
     [TestMethod]
-    public void SimpleTests()
+    public void SimpleTest()
     {
         var test = BasicFactory.Create<ITest>();
 
@@ -56,7 +57,7 @@ public class BakeryTests
     }
 
     [TestMethod]
-    public void WithInitTests() => BasicFactory.Create<ITestWithInit>();
+    public void WithInitTest() => BasicFactory.Create<ITestWithInit>();
 
     public struct TrivialComplexPropertyImplementation<Value, Container> : IPropertyImplementation<Value, Container, EmptyMixIn>
     {
@@ -68,8 +69,62 @@ public class BakeryTests
     }
 
     [TestMethod]
-    public void WithTrivialComplexTests() => BakeryConfiguration.Create(typeof(TrivialComplexPropertyImplementation<,>))
+    public void TrivialComplexTest() => BakeryConfiguration.Create(typeof(TrivialComplexPropertyImplementation<,>))
         .CreateBakery("Complex")
         .Create<ITest>()
         .Validate();
+
+
+
+
+    public struct TestMixin : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void NotifyPropertyChanged(Object o) => PropertyChanged?.Invoke(o, new PropertyChangedEventArgs(""));
+    }
+
+    public struct NotifyPropertyChangedPropertyImplementation<Value, Container> : IPropertyImplementation<Value, Container, TestMixin>
+        where Container : class
+    {
+        Value value;
+
+        public Value Get(Container self, ref TestMixin mixIn) => value;
+
+        public void Set(Container self, ref TestMixin mixIn, Value value)
+        {
+            this.value = value;
+
+            mixIn.NotifyPropertyChanged(self);
+        }
+    }
+
+    [TestMethod]
+    public void NotifyPropertyChangedTest()
+    {
+        var instance = BakeryConfiguration.Create(typeof(NotifyPropertyChangedPropertyImplementation<,>))
+            .CreateBakery("Complex")
+            .Create<ITest>();
+
+        var instanceAsNotifyPropertyChanged = instance as INotifyPropertyChanged;
+
+        var changeCount = 0;
+
+        Assert.IsNotNull(instanceAsNotifyPropertyChanged);
+
+        instanceAsNotifyPropertyChanged!.PropertyChanged += (o, e) =>
+        {
+            Assert.AreSame(instance, o);
+
+            ++changeCount;
+        };
+
+        Assert.AreEqual(0, changeCount);
+
+        instance.Value = "Foo";
+
+        Assert.AreEqual("Foo", instance.Value);
+
+        Assert.AreEqual(1, changeCount);
+    }
 }
