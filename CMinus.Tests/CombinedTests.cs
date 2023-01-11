f26f7d2cc99b5cbfb410723f4780bb164ee9f4fa
jtheisen <jens.theisen@github.com>
2023-01-11 13:14:10 +0100

    Insert old Moldinium test suite (minus the model tests)

---
+++ CMinus.Tests/CombinedTests.cs
@@ -0,0 +1,33 @@
+﻿using CMinus.Injection;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CMinus.Tests;
+
+[TestClass]
+public class CombinedTests
+{
+    IDependencyProvider provider;
+
+    public CombinedTests()
+    {
+        var services = new ServiceCollection();
+        services.AddSingleton(new RootService());
+
+        // Not used right now as we're blindly accepting all default constructible types anyway
+        var knownTypesProvider = new ConcreteDependencyProvider(typeof(ClassType));
+
+        provider = new CombinedDependencyProvider(
+            new ServiceProviderDependencyProvider(services.BuildServiceProvider()),
+            new AcceptingDefaultConstructiblesDependencyProvider(), // We really should only allow "baked" types to be blindly constructed
+            new BakeryDependencyProvider(new Bakery("TestBakery")),
+            new FactoryDependencyProvider(),
+            new ActivatorDependencyProvider(),
+            new InitSetterDependencyProvider()
+        );
+    }
+
+    [TestMethod]
+    public void InterfaceTypeWithParameterizedFactoryInstanceTest()
+        => provider.CreateInstance<InterfaceTypeWithParameterizedFactory>().Validate();
+}
---
+++ CMinus/Combined/WatchableBaking.cs
@@ -0,0 +1,8 @@
+﻿namespace CMinus;
+
+public struct WatchablePropertyImplementation<T> : IPropertyImplementation<T>
+{
+    Var<T> variable;
+
+    public T Value { get => variable.Value; set => variable.Value = value; }
+}
