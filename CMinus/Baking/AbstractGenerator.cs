b983935d5213a820630dd112c5989745717e086c
jtheisen <jens.theisen@github.com>
2023-01-20 23:57:06 +0100

    We can use delegates to call private implementations, but obviously this is annoying; it does work with Calli if a delegate has been used earlier (or probably anything that loads the implementation method)

--- CMinus/Baking/AbstractGenerator.cs
+++ CMinus/Baking/AbstractGenerator.cs
@@ -32,8 +32,6 @@ public abstract class AbstractGenerator
 
         var parameters = methodTemplate.GetParameters();
 
-        var rcms = methodTemplate.ReturnParameter.GetRequiredCustomModifiers();
-
         var methodBuilder = typeBuilder.DefineMethod(
             methodTemplate.Name,
             attributes,
---
+++ CMinus/Baking/CheekyCaller.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Reflection;
+
+namespace CMinus.Baking
+{
+    // some important experiment with Ldvirtfn that shows the safest way to call private implementations is to use delegates
+    internal static class CheekyCaller
+    {
+        static void Main()
+        {
+            Func<String> a = (new CInnocent() as IBase).Foo;
+
+            // this is required for the below to work - I guess the delegate internally assures
+            // that the private implementation is loaded
+            a();
+
+            Cheeky.CreateCaller<Func<IBase, String>>(typeof(IDerived), "Foo")(new CWithImpls());
+        }
+
+        public static class Cheeky
+        {
+            public static D CreateCaller<D>(Type type, String methodName)
+                where D : Delegate
+            {
+                return CreateCaller<D>(type.GetMethods(BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance).Where(m => m.Name == methodName || m.Name.EndsWith($".{methodName}")).Single());
+            }
+
+            public static D CreateCaller<D>(MethodInfo implementation)
+                where D : Delegate
+            {
+                var cheekyCallerMethod = new DynamicMethod(
+                    "ProxyDelegate",
+                    typeof(String),
+                    new Type[] { typeof(IBase) }
+                );
+
+                var il = cheekyCallerMethod.GetILGenerator();
+
+                il.Emit(OpCodes.Ldarg_0);
+                il.Emit(OpCodes.Ldvirtftn, implementation);
+                il.EmitCalli(OpCodes.Calli, CallingConventions.Any, implementation.ReturnType, implementation.GetParameters().Select(p => p.ParameterType).ToArray(), null);
+                il.Emit(OpCodes.Ret);
+
+                return (D)cheekyCallerMethod.CreateDelegate(typeof(D));
+            }
+        }
+
+        public interface IBase
+        {
+            String Foo() => "I";
+        }
+
+        public interface IDerived : IBase
+        {
+            String IBase.Foo() => "D";
+        }
+
+        class CInnocent : IDerived { }
+
+        class CWithImpls : IDerived
+        {
+            String IBase.Foo() => "C2";
+        }
+    }
+}
