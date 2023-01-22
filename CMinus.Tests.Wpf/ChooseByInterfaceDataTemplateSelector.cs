4fcaded8cc2476be26a38951342234e63525d038
jtheisen <jens.theisen@github.com>
2023-01-22 01:50:02 +0100

    Got data template choosing by interfaces working in some way

---
+++ CMinus.Tests.Wpf/ChooseByInterfaceDataTemplateSelector.cs
@@ -0,0 +1,28 @@
+﻿using CMinus.Injection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Markup;
+
+namespace CMinus.Tests.Wpf
+{
+    [ContentProperty("DataTemplates")]
+    public class ChooseByInterfaceDataTemplateSelector : DataTemplateSelector
+    {
+        public List<DataTemplate> DataTemplates { get; set; } = new List<DataTemplate>();
+
+        public override DataTemplate? SelectTemplate(Object? item, DependencyObject container)
+        {
+            if (item is null) return null;
+
+            var type = item.GetType();
+
+            var candidates = DataTemplates.Where(c => c.DataType is Type t && t.IsAssignableFrom(type)).ToArray();
+
+            return candidates.SingleOrDefault();
+        }
+    }
+}
+
