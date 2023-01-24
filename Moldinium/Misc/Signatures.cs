13f45bf6a139709612344839fdd0d6306b3fc101
jtheisen <jens.theisen@github.com>
2023-01-24 17:51:49 +0100

    Have to encode signatures ourselves after all

--- Moldinium/Misc/Signatures.cs
+++ Moldinium/Misc/Signatures.cs
@@ -1,9 +1,9 @@
-﻿using System;
-using System.Linq;
-using System.Reflection.Emit;
+﻿using System.Reflection.Emit;
 using System.Reflection;
 using System.Security.Cryptography;
 using System.Collections.Concurrent;
+using System.Reflection.Metadata;
+using System.Text;
 
 namespace Moldinium;
 
@@ -12,13 +12,32 @@ public struct Blob : IEquatable<Blob>
     Byte[] bytes;
     Int32 hash;
 
+    static HashAlgorithm hashAlgorithm;
+
+    static String[] algorithmsToTry = new[] { "MD5", "SHA1" }; 
+
+    static Blob()
+    {
+        foreach (var name in algorithmsToTry)
+        {
+            if (HashAlgorithm.Create(name) is HashAlgorithm found)
+            {
+                hashAlgorithm = found;
+
+                return;
+            }
+        }
+
+        throw new Exception($"The Platform supports none of these hash algorithms: {String.Join(", ", algorithmsToTry)}");
+    }
+
     public Blob(Byte[] bytes)
     {
         this.bytes = bytes;
 
-        var md5 = MD5.HashData(bytes);
+        var hashBytes = hashAlgorithm.ComputeHash(bytes);
 
-        hash = BitConverter.ToInt32(md5, 0);
+        hash = BitConverter.ToInt32(hashBytes, 0);
     }
 
     public static implicit operator Blob(Byte[] bytes) => new Blob(bytes);
@@ -162,4 +181,91 @@ public static class MethodSignatures
 
     public static EquatableMethodSignature GetSignature(MethodInfo method)
         => GetExtraMethodInfo(method).MethodNameAndSignature.Signature;
+}
+
+public abstract class AbstractSignatureHelper
+{
+    public abstract Byte[] GetSignature(MethodInfo methodInfo);
+}
+
+public class DotNetSignatureHelper : AbstractSignatureHelper
+{
+    public override byte[] GetSignature(MethodInfo method)
+    {
+        var s = SignatureHelper.GetMethodSigHelper(method.CallingConvention, method.ReturnType);
+
+        foreach (var p in method.GetParameters())
+        {
+            s.AddArgument(p.ParameterType, p.GetRequiredCustomModifiers(), p.GetOptionalCustomModifiers());
+        }
+
+        return s.GetSignature();
+    }
+}
+
+public class CustomSignatureHelper : AbstractSignatureHelper
+{
+    class SignatureEncoder
+    {
+        StringWriter writer = new StringWriter();
+
+        public String GetString() => writer.ToString();
+
+        public void EncodeMethod(MethodInfo method)
+        {
+            if (method.ReturnParameter is ParameterInfo returnParameter)
+            {
+                EncodeParameter(returnParameter, 0);
+            }
+
+            var paramters = method.GetParameters();
+
+            for (var i = 0; i < paramters.Length; ++i)
+            {
+                EncodeParameter(paramters[i], i + 1);
+            }
+        }
+
+        void EncodeParameter(ParameterInfo parameter, Int32 position)
+        {
+            writer.Write(position);
+            writer.Write('[');
+            EncodeType(parameter.ParameterType);
+            foreach (var m in parameter.GetRequiredCustomModifiers())
+            {
+                EncodeModifier(m, true);
+            }
+            foreach (var m in parameter.GetOptionalCustomModifiers())
+            {
+                EncodeModifier(m, false);
+            }
+            EncodeType(parameter.ParameterType);
+
+            writer.Write(']');
+        }
+
+        void EncodeModifier(Type modifier, Boolean isRequired)
+        {
+            writer.Write(isRequired ? 'r' : 'o');
+            EncodeType(modifier);
+        }
+
+        void EncodeType(Type type)
+        {
+            writer.Write('(');
+            writer.Write(type.Name);
+            writer.Write('|');
+            writer.Write(type.TypeHandle.Value.ToInt64());
+            writer.Write(')');
+        }
+    }
+
+    public override byte[] GetSignature(MethodInfo method)
+    {
+        var encoder = new SignatureEncoder();
+
+        encoder.EncodeMethod(method);
+
+        return Encoding.UTF8.GetBytes(encoder.GetString());
+    }
 }
